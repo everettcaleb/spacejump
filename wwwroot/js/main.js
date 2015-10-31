7472e892000ddbdb877ac08ebd257ee755852f50
Caleb Everett <everettcaleb95@gmail.com>
2015-10-30 21:25:50 -0400

    Added code to handle transforms from view to window coordinates (for positioning elements outside of the canvas)

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,5 +1,5 @@
 require([
   'js/viewport'
 ], function() {
-  //TODO!!!
+
 });
--- wwwroot/js/viewport.js
+++ wwwroot/js/viewport.js
@@ -2,24 +2,34 @@ define([
   'helpers'
 ], function(helpers) {
   var c = document.getElementById("canvas"),
-    vw = 0, vh = 0, vx = 0, ar = 4 / 3;
+    // width, height, x offset
+    vw = 1000, vh = 750, vx = 0,
+    // view scaling factors from view -> window
+    vws = 1, vhs = 1,
+    // constants
+    VIEW_WIDTH = 1000, VIEW_HEIGHT = 750, VIEW_ASPECT = 4 / 3,
+    exports = {};
 
   // Code to handle screen resizes and automatically scale and center canvas
   function onWindowResize() {
     var size = helpers.getWindowSize();
 
     // Check aspect ratio and calculate canvas size
-    if(size.width / size.height > ar) {
+    if(size.width / size.height > VIEW_ASPECT) {
       vh = size.height;
-      vw = size.height * ar;
+      vw = size.height * VIEW_ASPECT;
     }
     else {
       vw = size.width;
-      vh = size.width / ar;
+      vh = size.width / VIEW_ASPECT;
     }
 
     // Calculate x offset to center
     vx = (size.width - vw) / 2;
+
+    // Calculate scaling factors
+    vws = vw / VIEW_WIDTH;
+    vhs = vh / VIEW_HEIGHT;
 
     // Set style
     c.style.width = vw + "px";
@@ -27,6 +37,17 @@ define([
     c.style.left = vx + "px";
   }
 
+  // Function to transform vector from viewport coordinates (1000x750) to window coordinates
+  // Returns object {x, y}
+  exports.transformViewToWindow = function(x, y) {
+    return {
+      x: (x * vws) + vx,
+      y: y * vhs
+    };
+  }
+
+  // Main functionality
   onWindowResize(); // initialize canvas to initial size
   helpers.addEvent(window, 'resize', onWindowResize);
+  return exports;
 });
